466fbdd2101dcc71d341dd761897f882c6ba3e6f
angie <88114622+angiefn@users.noreply.github.com>
2023-12-12 12:08:54 -0800

    world scene setup

--- Player.gd
+++ Player.gd
@@ -14,7 +14,7 @@ func _process(delta):
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 	if Input.is_action_pressed("move_down"):
-		velocity.x += 1
+		velocity.y += 1
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * Speed
